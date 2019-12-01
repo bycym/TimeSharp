d02192f14310f4661f52d23deb2c130d66bce174
bycym <arcsibo@gmail.com>
2019-12-01 12:55:46 +0100

    ADD: Init

---
+++ TimeSharp/TimeSharp.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace TimeSharp
+{
+    partial class TimeSharp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TimeLabel = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.TimerTextBox = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.switchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopperToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TimeLabel
+            // 
+            this.TimeLabel.AutoSize = true;
+            this.TimeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.TimeLabel.Location = new System.Drawing.Point(12, 24);
+            this.TimeLabel.Name = "TimeLabel";
+            this.TimeLabel.Size = new System.Drawing.Size(212, 55);
+            this.TimeLabel.TabIndex = 0;
+            this.TimeLabel.Text = "00:00:00";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(230, 24);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(230, 53);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.TabIndex = 2;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(230, 83);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(75, 23);
+            this.ResetButton.TabIndex = 3;
+            this.ResetButton.Text = "button3";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // TimerTextBox
+            // 
+            this.TimerTextBox.Location = new System.Drawing.Point(12, 86);
+            this.TimerTextBox.Name = "TimerTextBox";
+            this.TimerTextBox.Size = new System.Drawing.Size(212, 20);
+            this.TimerTextBox.TabIndex = 4;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.switchToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(329, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // switchToolStripMenuItem
+            // 
+            this.switchToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stopperToolStripMenuItem,
+            this.timerToolStripMenuItem});
+            this.switchToolStripMenuItem.Name = "switchToolStripMenuItem";
+            this.switchToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.switchToolStripMenuItem.Text = "Switch";
+            // 
+            // stopperToolStripMenuItem
+            // 
+            this.stopperToolStripMenuItem.Name = "stopperToolStripMenuItem";
+            this.stopperToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.stopperToolStripMenuItem.Text = "Stopper";
+            this.stopperToolStripMenuItem.Click += new System.EventHandler(this.StopperToolStripMenuItem_Click);
+            // 
+            // timerToolStripMenuItem
+            // 
+            this.timerToolStripMenuItem.Name = "timerToolStripMenuItem";
+            this.timerToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.timerToolStripMenuItem.Text = "Timer";
+            this.timerToolStripMenuItem.Click += new System.EventHandler(this.TimerToolStripMenuItem_Click);
+            // 
+            // TimeSharp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 127);
+            this.Controls.Add(this.TimerTextBox);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.TimeLabel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "TimeSharp";
+            this.Text = "TimeSharp";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TimeLabel;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.TextBox TimerTextBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem switchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopperToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem timerToolStripMenuItem;
+    }
+}
+
---
+++ TimeSharp/TimeSharp.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeSharp
+{
+    public partial class TimeSharp : Form
+    {
+        enum FormType
+        {
+            Timer,
+            Stopper
+        };
+
+        private string appName = "TimeSharp";
+        private FormType Setting;
+        private int TimerTime;
+        private System.Windows.Forms.Timer ATimer;
+        private int Ticker;
+        private DateTime Dt;
+
+        public TimeSharp()
+        {
+            Timer();
+            InitializeComponent();
+            Setting = FormType.Timer;
+            LoadInterface();
+
+        }
+
+        private void LoadInterface()
+        {
+            switch (Setting)
+            {
+                case FormType.Timer:
+                    this.TimerTextBox.Visible = true;
+                    this.ResetButton.Text = "Set";
+                    this.Text = appName + " - Timer";
+                    break;
+                case FormType.Stopper:
+                    this.TimerTextBox.Visible = false;
+                    this.ResetButton.Text = "Reset";
+                    this.Text = appName + " - Stopper";
+                    break;
+            }
+        }
+
+        public void Timer()
+        {
+            Dt = new DateTime();
+            ATimer = new System.Windows.Forms.Timer();
+            ATimer.Tick += new EventHandler(OnTimedEvent);
+            ATimer.Interval = 1 * 1000;
+        }
+
+        private void OnTimedEvent(object sender, EventArgs e)
+        {
+            switch (Setting)
+            {
+                case FormType.Timer:
+                    {
+                        if (Ticker <= 0)
+                        {
+                            ATimer.Enabled = false;
+                            ShowNotification();
+                            Ticker = TimerTime;
+                            ShowTime();
+                        }
+                        else
+                        {
+                            --Ticker;
+                            ShowTime();
+                        }
+                    }
+                    break;
+                case FormType.Stopper:
+                    {
+                        ++Ticker;
+                        ShowTime();
+                    }
+                    break;
+            }
+        }
+
+        public void ShowTime()
+        {
+            this.TimeLabel.Text = Dt.AddSeconds(Ticker).ToString("HH:mm:ss");
+        }
+
+        private void StopperToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Setting = FormType.Stopper;
+            LoadInterface();
+        }
+
+        private void TimerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Setting = FormType.Timer;
+            LoadInterface();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            switch (Setting)
+            {
+                case FormType.Timer:
+                    //Ticker = TimerTime;
+                    break;
+
+                case FormType.Stopper:
+                    break;
+            }
+            ShowTime();
+            ATimer.Enabled = true;
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            
+            switch (Setting)
+            {
+                case FormType.Timer:
+                    if(ATimer.Enabled == false)
+                        Ticker = TimerTime;
+                    break;
+
+                case FormType.Stopper:
+                    break;
+            }
+            ATimer.Enabled = false;
+            ShowTime();
+        }
+
+        // Reset / Set button
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            ATimer.Enabled = false;
+            TimerTime = 0;
+            int tmp = 0;
+            switch (Setting)
+            {
+                case FormType.Timer:
+                    if (Int32.TryParse(this.TimerTextBox.Text, out tmp))
+                    {
+                        TimerTime = Ticker = tmp;
+                    }
+                    else
+                    {
+                        string TimerBoxText = this.TimerTextBox.Text;
+                        if (TimerBoxText.Contains(':'))
+                        {
+                            string[] tokens = TimerBoxText.Split(':');
+                            if (tokens.Length == 2)
+                            {
+                                if (Int32.TryParse(tokens[0], out tmp))
+                                {
+                                    TimerTime += tmp * 60 * 60;
+                                    if (Int32.TryParse(tokens[1], out tmp))
+                                    {
+                                        TimerTime += tmp * 60;
+                                        Ticker = TimerTime;
+                                    }
+                                }
+                            }
+                            else if(tokens.Length == 3)
+                            {
+                                if (Int32.TryParse(tokens[0], out tmp))
+                                {
+                                    TimerTime += tmp * 60 * 60;
+                                    if (Int32.TryParse(tokens[1], out tmp))
+                                    {
+                                        TimerTime += tmp * 60;
+                                        if (Int32.TryParse(tokens[2], out tmp))
+                                        {
+                                            TimerTime += tmp;
+                                            Ticker = TimerTime;
+                                        }
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show(this, "Time should be: hh:mm or hh:mm:ss", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show(this, "Time should be: hh:mm or hh:mm:ss", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        
+                    }
+                    break;
+                case FormType.Stopper:
+                    Ticker = 0;
+                    break;
+            }
+            ShowTime();
+        }
+
+        private void ShowNotification()
+        {
+            this.WindowState = FormWindowState.Minimized;
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+
+            MessageBox.Show(this, "Timer ended " + DateTime.Now.ToString("HH:mm:ss")
+                , "Ended", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            var notification = new System.Windows.Forms.NotifyIcon()
+            {
+                Visible = true,
+                Icon = System.Drawing.SystemIcons.Information,
+                // optional - BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info,
+                // optional - BalloonTipTitle = "My Title",
+                BalloonTipText = "My long description...",
+            };
+
+            // Display for 5 seconds.
+            notification.ShowBalloonTip(5000);
+
+            // This will let the balloon close after it's 5 second timeout
+            // for demonstration purposes. Comment this out to see what happens
+            // when dispose is called while a balloon is still visible.
+            //Thread.Sleep(10000);
+
+            // The notification should be disposed when you don't need it anymore,
+            // but doing so will immediately close the balloon if it's visible.
+            notification.Dispose();
+        }
+    }
+}
